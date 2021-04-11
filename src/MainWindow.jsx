893709f6cd9bfa75a3911f24bfcf78bc9e9946ab
ismilawd <ismilawd@gmail.com>
2021-04-11 08:52:29 +0430

    move frame component

--- src/MainWindow.jsx
+++ src/MainWindow.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import WindowFrame from "./components/app/frame.jsx";
+import WindowFrame from "./components/app/frame/index.jsx";
 export default class MainWindow extends Component {
     render() {
         return <>
