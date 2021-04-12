4173c6522eaf3d0d50107c1f7d20bf69d9f6b505
ismilawd <ismilawd@gmail.com>
2021-04-12 22:09:00 +0430

    Messing Area Header

--- src/MainWindow.jsx
+++ src/MainWindow.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import WindowFrame from "./components/app/frame/WindowFrame.jsx";
 import ChatList from './components/app/chat/ChatList.jsx';
+import MessagePanel from './components/app/messaging/MessagePanel.jsx';
 
 export default class MainWindow extends Component {
     render() {
@@ -8,6 +9,7 @@ export default class MainWindow extends Component {
             <WindowFrame></WindowFrame>
             <div className="app-container">
                 <ChatList></ChatList>
+                <MessagePanel></MessagePanel>
             </div>
         </>
     }
